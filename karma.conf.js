59bb558b405901291f952e23d64ec1caee59b6e8
Alan Burchette <alanburchette1@gmail.com>
2015-03-19 21:57:21 -0400

    Adding ngFileToJson and removing unecessary lines from Karma

--- karma.conf.js
+++ karma.conf.js
@@ -17,12 +17,11 @@ module.exports = function(config) {
       'client/bower_components/angular-resource/angular-resource.js',
       'client/bower_components/angular-cookies/angular-cookies.js',
       'client/bower_components/angular-sanitize/angular-sanitize.js',
-      'client/bower_components/angular-route/angular-route.js',
       'client/bower_components/lodash/dist/lodash.compat.js',
       'client/bower_components/angular-socket-io/socket.js',
       'client/bower_components/angular-ui-router/release/angular-ui-router.js',
+      'client/bower_components/angular-file-to-json/angular-file-to-json.js',
       'client/app/app.js',
-      'client/app/app.coffee',
       'client/app/**/*.js',
       'client/app/**/*.coffee',
       'client/components/**/*.js',
@@ -35,8 +34,7 @@ module.exports = function(config) {
 
     preprocessors: {
       '**/*.jade': 'ng-jade2js',
-      '**/*.html': 'html2js',
-      '**/*.coffee': 'coffee',
+      '**/*.html': 'html2js'
     },
 
     ngHtml2JsPreprocessor: {
